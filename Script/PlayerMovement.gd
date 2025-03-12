bd337a7dd07ad4e086b49b7741ddd303a1c597af
MuffinMan <91591598+NoahGram@users.noreply.github.com>
2025-03-12 21:21:42 +0100

    Upgrade to 4.4 / A & D Movement Added

--- Script/PlayerMovement.gd
+++ Script/PlayerMovement.gd
@@ -34,7 +34,7 @@ func _physics_process(delta: float) -> void:
 			velocity.y = JUMP_VELOCITY
 
 		# Get input direction and handle movement/deceleration
-		var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+		var input_dir := Input.get_vector("move_left", "move_right", "ui_up", "ui_down")
 		var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 
 		velocity.x = move_toward(velocity.x, 12.5, SPEED)
